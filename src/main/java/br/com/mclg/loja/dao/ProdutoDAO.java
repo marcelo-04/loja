039f60a192a995a49c708a4f809841734e536fb1
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-04-28 21:10:28 -0300

    Consultando entidades com JPQL.

--- src/main/java/br/com/mclg/loja/dao/ProdutoDAO.java
+++ src/main/java/br/com/mclg/loja/dao/ProdutoDAO.java
@@ -1,5 +1,7 @@
 package br.com.mclg.loja.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.mclg.loja.modelo.Produto;
@@ -15,5 +17,14 @@ public class ProdutoDAO {
 	public void cadastrar(Produto produto) {
 		
 		this.em.persist(produto);
+	}
+	
+	public Produto buscarPorId (Long id) {
+		return em.find(Produto.class, id);
+	}
+	
+	public List<Produto> buscarTodos() {
+		String jpql = "SELECT p FROM Produto p";
+		return em.createQuery(jpql, Produto.class).getResultList();
 	}
 }
--- src/main/java/br/com/mclg/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/mclg/loja/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.mclg.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -12,35 +13,33 @@ import br.com.mclg.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
+		cadastrarProduto();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDao = new ProdutoDAO(em);
 		
-		Categoria celulares = new Categoria("CELULARES");
+		Produto p = produtoDao.buscarPorId(1l);
+		System.out.println(p.getPreco());
 		
-		//Produto celular = new Produto("Xiome Redmi", "Muito bom", new BigDecimal("800"), celulares);
+		List<Produto> todos = produtoDao.buscarTodos();
+		//todos.forEach(p2 -> System.out.println(p.getNome()));
+	}
+
+	private static void cadastrarProduto() {
+		Categoria celulares = new Categoria("CELULARES");	
+		Produto celular = new Produto("Xiome Redmi", "Muito bom", new BigDecimal("800"), celulares);
 		
 		EntityManager em = JPAUtil.getEntityManager();
-		//ProdutoDAO produtoDao = new ProdutoDAO(em);
-		//CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+		ProdutoDAO produtoDao = new ProdutoDAO(em);
+		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 		
 		em.getTransaction().begin();
 		
-		em.persist(celulares);
-		celulares.setNome("XPTO");
-		//categoriaDAO.cadastrar(celulares);
-		//produtoDao.cadastrar(celular);
+		categoriaDAO.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
 		
-		em.flush();
-		em.clear();
-		
-		//em.getTransaction().commit();
-		celulares = em.merge(celulares);
-		celulares.setNome("1234");
-		em.flush();
-		em.clear();
-		em.remove(celulares);
-		em.flush();
-		
-		//em.close();
+		em.getTransaction().commit();
+		em.close();
 	}
 
 }
