6a1b54ebeb31f94de3ac28935c7aab1470cbed38
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-05-03 14:01:22 -0300

    Relacionamento many-to-many.

--- src/main/java/br/com/mclg/loja/modelo/Pedido.java
+++ src/main/java/br/com/mclg/loja/modelo/Pedido.java
@@ -2,12 +2,14 @@ package br.com.mclg.loja.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,12 +24,15 @@ public class Pedido {
 
 	@ManyToOne
 	private Cliente cliente;
+	
+	@OneToMany
+	private List<ItemPedido> itens;
+	
+	public Pedido() {
+	}
 
 	public Pedido(Cliente cliente) {
 		this.cliente = cliente;
-	}
-
-	public Pedido() {
 	}
 
 	public Long getId() {
