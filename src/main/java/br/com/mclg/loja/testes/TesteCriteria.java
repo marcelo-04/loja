c2428e30464c870a2be946c9ea1eacf9ef145947
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-05-17 22:04:57 -0300

    COnsultas com Criteria API.

---
+++ src/main/java/br/com/mclg/loja/testes/TesteCriteria.java
@@ -0,0 +1,49 @@
+package br.com.mclg.loja.testes;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.EntityManager;
+
+import br.com.mclg.loja.dao.CategoriaDAO;
+import br.com.mclg.loja.dao.ProdutoDAO;
+import br.com.mclg.loja.modelo.Categoria;
+import br.com.mclg.loja.modelo.Produto;
+import br.com.mclg.loja.util.JPAUtil;
+
+public class TesteCriteria {
+	
+	public static void main(String[] args) {
+		popularBancoDeDados();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDAO = new ProdutoDAO(em);
+		produtoDAO.buscarPorParametrosComCriteria(null, null, LocalDate.now());
+	}
+	
+	private static void popularBancoDeDados() {
+		Categoria celulares = new Categoria("CELULARES");
+		Categoria videogames = new Categoria("VIDEOGAMES");
+		Categoria informatica = new Categoria("INFORMATICA");
+		
+		Produto celular = new Produto("Xiaome Redmi", "Muito bom", new BigDecimal("800"), celulares);
+		Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("8000"), videogames);
+		Produto macbook = new Produto("Macbook", "Macboo pro retina", new BigDecimal("14000"), informatica);
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDao = new ProdutoDAO(em);
+		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+	
+		em.getTransaction().begin();
+		
+		categoriaDAO.cadastrar(celulares);
+		categoriaDAO.cadastrar(videogames);
+		categoriaDAO.cadastrar(informatica);
+		
+		produtoDao.cadastrar(celular);
+		produtoDao.cadastrar(videogame);
+		produtoDao.cadastrar(macbook);
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+}
